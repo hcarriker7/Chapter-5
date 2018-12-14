f0cd1a5e26adb5d78bffbbf98c33ec86cf38a303
hcarriker7 <45023562+hcarriker7@users.noreply.github.com>
2018-12-14 13:43:21 -0600

    Add files via upload

---
+++ DisplayMultiplicationTable/DisplayMultiplicationTable/Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace DisplayMultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int a = 2;
+            int b = 3;
+            int c = 4;
+            int d = 5;
+            int e = 6;
+            int f = 7;
+            int g = 8;
+            int h = 9;
+            int j = 10;
+            int i;
+            int k = 0;
+
+            WriteLine("Welcome to the multiplication table. ");
+            WriteLine("It multiplies numbers 1 though 10 in order. ");
+
+            for (i = 1; i <= 1; i++)
+            {
+                WriteLine("-- 1 * num --");
+                Write(" " + x * x + " ");
+                k++;
+                Write(" " + x * a + " ");
+                k++;
+                Write(" " + x * b + " ");
+                k++;
+                Write(" " + x * c + " ");
+                k++;
+                Write(" " + x * d + " ");
+                k++;
+                Write(" " + x * e + " ");
+                k++;
+                Write(" " + x * f + " ");
+                k++;
+                Write(" " + x * g + " ");
+                k++;
+                Write(" " + x * h + " ");
+                k++;
+                Write(" " + x * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 2 * num --");
+                Write(" " + a * x + " ");
+                Write(" " + a * a + " ");
+                k++;
+                Write(" " + a * b + " ");
+                k++;
+                Write(" " + a * c + " ");
+                k++;
+                Write(" " + a * d + " ");
+                k++;
+                Write(" " + a * e + " ");
+                k++;
+                Write(" " + a * f + " ");
+                k++;
+                Write(" " + a * g + " ");
+                k++;
+                Write(" " + a * h + " ");
+                k++;
+                Write(" " + a * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 3 * num --");
+                Write(" " + b * x + " ");
+                Write(" " + b * a + " ");
+                k++;
+                Write(" " + b * b + " ");
+                k++;
+                Write(" " + b * c + " ");
+                k++;
+                Write(" " + b * d + " ");
+                k++;
+                Write(" " + b * e + " ");
+                k++;
+                Write(" " + b * f + " ");
+                k++;
+                Write(" " + b * g + " ");
+                k++;
+                Write(" " + b * h + " ");
+                k++;
+                Write(" " + b * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 4 * num --");
+                Write(" " + c * x + " ");
+                Write(" " + c * a + " ");
+                k++;
+                Write(" " + c * b + " ");
+                k++;
+                Write(" " + c * c + " ");
+                k++;
+                Write(" " + c * d + " ");
+                k++;
+                Write(" " + c * e + " ");
+                k++;
+                Write(" " + c * f + " ");
+                k++;
+                Write(" " + c * g + " ");
+                k++;
+                Write(" " + c * h + " ");
+                k++;
+                Write(" " + c * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 5 * num --");
+                Write(" " + d * x + " ");
+                Write(" " + d * a + " ");
+                k++;
+                Write(" " + d * b + " ");
+                k++;
+                Write(" " + d * c + " ");
+                k++;
+                Write(" " + d * d + " ");
+                k++;
+                Write(" " + d * e + " ");
+                k++;
+                Write(" " + d * f + " ");
+                k++;
+                Write(" " + d * g + " ");
+                k++;
+                Write(" " + d * h + " ");
+                k++;
+                Write(" " + d * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 6 * num --");
+                Write(" " + e * x + " ");
+                Write(" " + e * a + " ");
+                k++;
+                Write(" " + e * b + " ");
+                k++;
+                Write(" " + e * c + " ");
+                k++;
+                Write(" " + e * d + " ");
+                k++;
+                Write(" " + e * e + " ");
+                k++;
+                Write(" " + e * f + " ");
+                k++;
+                Write(" " + e * g + " ");
+                k++;
+                Write(" " + e * h + " ");
+                k++;
+                Write(" " + e * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 7 * num --");
+                Write(" " + f * x + " ");
+                Write(" " + f * a + " ");
+                k++;
+                Write(" " + f * b + " ");
+                k++;
+                Write(" " + f * c + " ");
+                k++;
+                Write(" " + f * d + " ");
+                k++;
+                Write(" " + f * e + " ");
+                k++;
+                Write(" " + f * f + " ");
+                k++;
+                Write(" " + f * g + " ");
+                k++;
+                Write(" " + f * h + " ");
+                k++;
+                Write(" " + f * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 8 * num --");
+                Write(" " + g * x + " ");
+                Write(" " + g * a + " ");
+                k++;
+                Write(" " + g * b + " ");
+                k++;
+                Write(" " + g * c + " ");
+                k++;
+                Write(" " + g * d + " ");
+                k++;
+                Write(" " + g * e + " ");
+                k++;
+                Write(" " + g * f + " ");
+                k++;
+                Write(" " + g * g + " ");
+                k++;
+                Write(" " + g * h + " ");
+                k++;
+                Write(" " + g * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 9 * num --");
+                Write(" " + h * x + " ");
+                Write(" " + h * a + " ");
+                k++;
+                Write(" " + h * b + " ");
+                k++;
+                Write(" " + h * c + " ");
+                k++;
+                Write(" " + h * d + " ");
+                k++;
+                Write(" " + h * e + " ");
+                k++;
+                Write(" " + h * f + " ");
+                k++;
+                Write(" " + h * g + " ");
+                k++;
+                Write(" " + h * h + " ");
+                k++;
+                Write(" " + h * j + "                                                                                          ");
+                k++;
+
+                WriteLine("-- 10 * num --");
+                Write(" " + j * x + " ");
+                Write(" " + j * a + " ");
+                k++;
+                Write(" " + j * b + " ");
+                k++;
+                Write(" " + j * c + " ");
+                k++;
+                Write(" " + j * d + " ");
+                k++;
+                Write(" " + j * e + " ");
+                k++;
+                Write(" " + j * f + " ");
+                k++;
+                Write(" " + j * g + " ");
+                k++;
+                Write(" " + j * h + " ");
+                k++;
+                Write(" " + j * j + "                                                                                          ");
+                k++;
+            }
+
+     
+        }
+    }
+}
---
+++ OddNums/OddNums/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace OddNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello this displays all the odd numbers 1 through 99 because apparently people dont know them");
+
+            Write("1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 36 37 39 41 43 45 47 49 51 53 55 57 59 61 " +
+                "63 65 67 69 71 73 75 77 79 81 83 85 87 89 91 93 95 97 99 ");
+
+        }
+    }
+}
